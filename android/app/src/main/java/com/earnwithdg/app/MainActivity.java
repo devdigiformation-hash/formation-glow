f80a37477fa56aa236a7b4b1048f82d7bec3192c
devdigiformation-hash <dev.digiformation@gmail.com>
2026-06-13 07:16:04 -0700

    Modify MainActivity to launch app URL
    Updated MainActivity to open a URL in the browser on launch.

--- android/app/src/main/java/com/earnwithdg/app/MainActivity.java
+++ android/app/src/main/java/com/earnwithdg/app/MainActivity.java
@@ -1,5 +1,28 @@
 package com.earnwithdg.app;
 
-import com.getcapacitor.BridgeActivity;
+import android.app.Activity;
+import android.content.Intent;
+import android.net.Uri;
+import android.os.Bundle;
+import android.widget.Toast;
 
-public class MainActivity extends BridgeActivity {}
+public class MainActivity extends Activity {
+    private static final String APP_URL = "https://project--decc67c2-b397-453a-98ef-1296404f6cba.lovable.app";
+
+  @Override
+    protected void onCreate(Bundle savedInstanceState) {
+          super.onCreate(savedInstanceState);
+          openAppInBrowser();
+          finish();
+    }
+
+  private void openAppInBrowser() {
+        Intent intent = new Intent(Intent.ACTION_VIEW, Uri.parse(APP_URL));
+        intent.addCategory(Intent.CATEGORY_BROWSABLE);
+        try {
+                startActivity(intent);
+        } catch (Exception error) {
+                Toast.makeText(this, "Unable to open Earn with DG", Toast.LENGTH_LONG).show();
+        }
+  }
+}
